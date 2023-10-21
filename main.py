baa270fa70d298e643a69a845fbf57690a62f863
Odara Valença <odaravalenca@gmail.com>
2023-10-21 15:32:12 -0300

    feat: adicionar novo registro

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+listaEstudantes = []
+estudante = {}
+
+# Adicionar um novo registro de estudante (nome, ID e notas)
+def addEstudante():
+    estudante['nome'] = str(input('Digite o nome do estudante: '))
+    estudante['id'] = str(input('Digite o id do estudante: '))
+    notas = str(input('Digite as notas do estudante (separadas por espaço): '))
+    
+    # tratamento para separar e converter as notas
+    notas_separadas = notas.split(' ')
+    notas_int = [float(nota) for nota in notas_separadas]
+    estudante['notas'] = notas_int
+
+    listaEstudantes.append(estudante.copy())
 
 # Menu principal
 
@@ -17,6 +32,9 @@ while True:
     escolha = str(input('Digite sua escolha (1-7) '))
 
     match escolha:
+        case '1':
+            addEstudante()
+            print(f'Registro de {estudante["nome"]} adicionado com sucesso!')
         case '7':
             print('Até logo!')
             break
